79944bace84eff5cbbb8f6663459ae5d0b8953d0
lpt <141250073@smail.nju.edu.cn>
2016-07-08 18:32:24 +0800

    接口和数据库设计修改
    Signed-off-by: lpt <141250073@smail.nju.edu.cn>

--- src/main/java/blservice/ReviewBlService.java
+++ src/main/java/blservice/ReviewBlService.java
@@ -1,8 +1,11 @@
 package blservice;
 
+import java.util.List;
+
 import vo.TaskVO;
 
 public interface ReviewBlService {
 	public void saveReviewInfo(TaskVO vo);
+	public List<TaskVO> geTaskList(String userName);
 }
                  
