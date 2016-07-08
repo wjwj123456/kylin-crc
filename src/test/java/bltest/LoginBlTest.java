e36c337c5c66c160ffd862e599c4a44922f76b47
ldk14 <141250075@smail.nju.edu.cn>
2016-07-08 11:50:15 +0800

    登录 测试用例

---
+++ src/test/java/bltest/LoginBlTest.java
@@ -0,0 +1,32 @@
+package bltest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import data.LoginDataImpl;
+
+public class LoginBlTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+	private LoginDataImpl loginDataImpl = new LoginDataImpl();
+	@Test
+	public void testCreateAccount() {
+	//	assertEquals(true,loginDataImpl.createAccount(userName, email, password));
+	}
+
+	@Test
+	public void testVerifyAccount() throws ClassNotFoundException, SQLException {
+		assertEquals(1,loginDataImpl.verifyAccount("crc", "123") );
+	}
+
+	@Test
+	public void testChangePassword() {
+	//	assertEquals(true,loginDataImpl.changeAccount(userName, password));
+	}
+}
