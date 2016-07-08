7b62399150a498d54ad0d4465748d28efac0adaf
hx14 <hx14@software.nju.edu.cn>
2016-07-08 11:40:34 +0800

    Development

---
+++ src/main/java/data/DBManager.java
@@ -0,0 +1,30 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBManager {
+
+	public static Connection connect() throws SQLException, ClassNotFoundException {		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		String url = "jdbc:mysql://115.28.33.208:3306/crc";
+		String username = "ykk2";
+		String password = "123qwe";
+		
+		Connection connection = DriverManager.getConnection(url, username, password);
+		return connection;
+	}
+	
+	public static void stopAll(ResultSet rSet, Statement statement, Connection connection) throws SQLException {
+		if(rSet != null) 
+			rSet.close();
+		if(statement != null)
+			statement.close();
+		if(connection!=null) 
+			connection.close();
+	}
+}
