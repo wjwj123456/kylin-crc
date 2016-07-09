5dcdf7d0794eeed6a3d964551681d5fe69bac0c7
ldk14 <141250075@smail.nju.edu.cn>
2016-07-09 10:59:07 +0800

    review test

---
+++ src/test/java/bltest/ReviewBlTest.java
@@ -0,0 +1,35 @@
+package bltest;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import bl.ReviewBlImpl;
+import tools.Tools;
+import vo.TaskVO;
+import vo.Type;
+
+public class ReviewBlTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	private ReviewBlImpl reviewBlImpl = new ReviewBlImpl();
+	@Test
+	public void testSaveReviewInfo() {
+		TaskVO vo = new TaskVO("crc", "crc1", Type.code, "¹¤³Ì", "ÎÞ", Tools.stringToDate("2016-08-08 12:12:12"), 0);
+//		assertEquals(0,reviewBlImpl.saveReviewInfo(vo) );
+
+	}
+
+	@Test
+	public void testGeTaskList() {
+		assertNotEquals(0, reviewBlImpl.geTaskList("crc").size());
+		assertEquals(0, reviewBlImpl.geTaskList("fdjsjf").size());
+	}
+
+}
