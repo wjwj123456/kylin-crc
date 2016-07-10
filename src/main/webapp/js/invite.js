9ec40566b1379271fdfbb693991b443ec8ca09a2
bedisdover <sym14@software.nju.edu.cn>
2016-07-10 15:56:38 +0800

    Develop: search

--- src/main/webapp/js/invite.js
+++ src/main/webapp/js/invite.js
@@ -9,6 +9,10 @@ $(function() {
 	});
 });
 
+/**
+ * invite 
+ * @returns
+ */
 function invite() {
 	
 	jQuery.ajax({
@@ -22,27 +26,32 @@ function invite() {
 }
 
 $(function() {
+	var subjects = ['PHP', 'MySQL', 'SQL', 'PostgreSQL', 'HTML', 'CSS', 'HTML5', 'CSS3', 'JSON'];
+	$('#searchName').typeahead({source: subjects})
+	alert(1)
 	// 初始化typeahead
-	$('#searchName').typeahead({
-		source : function(query, process) {
-			// query是输入值
-			jQuery.getJSON('/Search/GetHotSearchItems', {
-				"query" : query
-			}, function(data) {
-				process(data);
-			});
-		},
-		updater : function(item) {
-			return item.replace(/<a(.+?)<\/a>/, ""); // 这里一定要return，否则选中不显示
-		},
-		afterSelect : function(item) {
+//$('#searchName').typeahead({
+//		source : function(query, process) {
+//			// query是输入值
+//			jQuery.getJSON('/crc/SearchServlet', {
+//				"query" : query
+//			}, function(data) {
+//				process(data);
+//			});
+//		},
+//		source: function(query, process) {
+//	         return ["Deluxe Bicycle", "Super Deluxe Trampoline", "Super Duper Scooter"];
+//	    },
+//		updater : function(item) {
+//			return item.replace(/<a(.+?)<\/a>/, ""); // 这里一定要return，否则选中不显示
+//		},
+//		afterSelect : function(item) {
 			// 选择项之后的时间，item是当前选中的项
-			alert(item);
-		},
-		items : 8, // 显示8条
-		delay : 500
-	// 延迟时间
-	});
+//			alert(item);
+//		},
+//		items : 8, // 显示8条
+//		delay : 500 // 延迟时间
+//	});
 });
 
 /**
@@ -76,5 +85,6 @@ function isUnique(obj) {
  * @returns
  */
 function displayUser(users) {
-	console.log(users)
+//	console.log(users)
+//	$('#searchName').val(users[0].name)
 }
