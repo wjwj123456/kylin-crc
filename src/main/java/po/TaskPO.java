7065cf0259d7e4a0f94e6d35b3ce904ed0bad153
ldk14 <141250075@smail.nju.edu.cn>
2016-07-11 15:48:38 +0800

    fix bug

--- src/main/java/po/TaskPO.java
+++ src/main/java/po/TaskPO.java
@@ -1,6 +1,5 @@
 package po;
 
-
 import java.util.Date;
 
 import vo.Type;
@@ -13,6 +12,7 @@ public class TaskPO {
 	String describe;
 	Date deadline;
 	int state;
+
 	public TaskPO(String userName, String taskName, Type type, String project, String describe, Date deadline,
 			int state) {
 		super();
@@ -24,60 +24,72 @@ public class TaskPO {
 		this.deadline = deadline;
 		this.state = state;
 	}
-	
-	
-	
-	
+
 	public TaskPO(String userName, String taskName) {
 		super();
 		this.userName = userName;
 		this.taskName = taskName;
 	}
 
+	public TaskPO() {
+		super();
 
-
+	}
 
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getTaskName() {
 		return taskName;
 	}
+
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
+
 	public Type getType() {
 		return type;
 	}
+
 	public void setType(Type type) {
 		this.type = type;
 	}
+
 	public String getProject() {
 		return project;
 	}
+
 	public void setProject(String project) {
 		this.project = project;
 	}
+
 	public String getDescribe() {
 		return describe;
 	}
+
 	public void setDescribe(String describe) {
 		this.describe = describe;
 	}
+
 	public Date getDeadline() {
 		return deadline;
 	}
+
 	public void setDeadline(Date deadline) {
 		this.deadline = deadline;
 	}
+
 	public int getState() {
 		return state;
 	}
+
 	public void setState(int state) {
 		this.state = state;
 	}
-	
+
 }
