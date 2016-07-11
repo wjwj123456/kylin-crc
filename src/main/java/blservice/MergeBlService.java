75acc8756ae9baabed8a799b2bb1555f9ae4bdcc
lpt <141250073@smail.nju.edu.cn>
2016-07-11 15:03:10 +0800

    Signed-off-by: lpt <141250073@smail.nju.edu.cn>

---
+++ src/main/java/blservice/MergeBlService.java
@@ -0,0 +1,26 @@
+  /**
+ * @author       lpt14
+ * @version      V1.0
+ */
+package blservice;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import po.ReportPO;
+import vo.ReportVO;
+
+/**
+* TODO: （类描述）
+*
+* @author lpt14
+* @since 2016年7月11日
+* @see
+*/
+public interface MergeBlService {
+	public boolean canMerge(String taskName) ;
+	public List<ReportVO> mergeReport(String taskName);
+	public void saveMergeReport(List<ReportVO> reportList);
+	
+	
+}
---
+++ src/test/java/bltest/MergeBlTest.java
@@ -0,0 +1,16 @@
+  /**
+ * @author       lpt14
+ * @version      V1.0
+ */
+package bltest;
+
+/**
+* TODO: （类描述）
+*
+* @author lpt14
+* @since 2016年7月11日
+* @see
+*/
+public class MergeBlTest {
+	
+}
