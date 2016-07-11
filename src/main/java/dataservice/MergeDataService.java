e49aebb655f95deb1ad1e51ef9092abd232b05c9
lpt <141250073@smail.nju.edu.cn>
2016-07-11 09:52:05 +0800

    Signed-off-by: lpt <141250073@smail.nju.edu.cn>

---
+++ src/main/java/dataservice/MergeDataService.java
@@ -0,0 +1,28 @@
+  /**
+ * @author       lpt14
+ * @version      V1.0
+ */
+package dataservice;
+
+import java.util.List;
+
+/**
+* TODO: （类描述）
+*
+* @author lpt14
+* @since 2016年7月10日
+* @see
+*/
+public interface MergeDataService {
+	
+//		public boolean canMerge(String userName,String taskName);
+		
+//		public List<ReportPO> mergeReport(String userNme,String taskName);
+		
+		
+		
+		
+		
+	
+
+}
