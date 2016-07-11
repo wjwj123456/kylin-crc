30b3534de87366cfb4085a5dadf241f7619503fc
bedisdover <sym14@software.nju.edu.cn>
2016-07-11 13:06:31 +0800

    Develop: data

--- src/main/java/filter/CRCFilter.java
+++ src/main/java/filter/CRCFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import bl.InviteBlImpl;
 import vo.TaskVO;
@@ -39,12 +40,30 @@ public class CRCFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
-		String userName = (String) req.getSession().getAttribute("username");
+		HttpSession session = req.getSession();
+
+		String userName = (String) session.getAttribute("username");
 
 		InviteBlImpl invite = new InviteBlImpl();
-		List<TaskVO> invitationList = invite.getInvitationInfo(userName);
 
-		req.getSession().setAttribute("invitationList", invitationList);
+		// 用户收到的邀请
+		List<TaskVO> invitationList = invite.getInvitationInfo(userName);
+		session.setAttribute("invitationList", invitationList);
+
+		// 正在进行中的任务
+		List<TaskVO> runningTask = invite.getAllDoingTask(userName);
+		session.setAttribute("runningTask", runningTask);
+
+		// 历史任务
+		List<TaskVO> historyTask = invite.getAllCompleteTask(userName);
+		session.setAttribute("historyTask", historyTask);
+
+		for (TaskVO task : runningTask) {
+			// 接受邀请的评审者
+			session.setAttribute("agree_" + task.getTaskName(), invite.getAgreeUser(task.getTaskName()));
+			// 未接受邀请
+			session.setAttribute("disagree_" + task.getTaskName(), invite.getDisagreeUser(task.getTaskName()));
+		}
 
 		chain.doFilter(request, response);
 	}
