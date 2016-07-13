18f68b17774e6b1aecddc00b0f80fa835704b45d
ldk14 <141250075@smail.nju.edu.cn>
2016-07-13 10:13:47 +0800

    方法

--- src/main/java/dataservice/ReportDataService.java
+++ src/main/java/dataservice/ReportDataService.java
@@ -5,20 +5,30 @@ import java.util.List;
 
 import po.ReportPO;
 
-
 public interface ReportDataService {
 	/**
 	 * ldk
+	 * 
 	 * @param vos
-	 * @return 0: 创建成功        1：失败
+	 * @return 0: 创建成功 1：失败
 	 */
-	public int createReport(List<ReportPO> pos)throws ClassNotFoundException, SQLException;
-	
+	public int createReport(List<ReportPO> pos) throws ClassNotFoundException, SQLException;
+
 	/**
-	 * ldk
-	 * 设置完成一份评审报告的时间
+	 * ldk 设置完成一份评审报告的时间
+	 * 
 	 * @param time
-	 * @return 0:设置成功        1：字段重复    2:失败
+	 * @return 0:设置成功 1：字段重复 2:失败
 	 */
-	public int setCompleteTime(String taskName, String reviewerName ,double time)throws ClassNotFoundException, SQLException;
+	public int setCompleteTime(String taskName, String reviewerName, double time)
+			throws ClassNotFoundException, SQLException;
+
+	/**
+	 * ldk get all reports by taskname *
+	 * 
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public List<ReportPO> getAllReportsByTaskName(String taskName) throws ClassNotFoundException, SQLException;
 }
