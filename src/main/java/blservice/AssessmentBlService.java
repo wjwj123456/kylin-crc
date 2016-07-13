c1212da518ff8fdf9a7e985dedbff1e37d789627
ldk14 <141250075@smail.nju.edu.cn>
2016-07-13 14:49:46 +0800

    a

--- src/main/java/blservice/AssessmentBlService.java
+++ src/main/java/blservice/AssessmentBlService.java
@@ -2,4 +2,11 @@ package blservice;
 
 public interface AssessmentBlService {
 
+	/**
+	 * ldk get the history assessment fault counts
+	 * 
+	 * @param taskName
+	 * @return
+	 */
+	public double[] getHistoryAssessmentValues(String taskName);
 }
