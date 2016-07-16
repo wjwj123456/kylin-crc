36526ea043b4af8d22558e3be2d79fb2c05bbb74
lpt <141250073@smail.nju.edu.cn>
2016-07-16 10:27:58 +0800

    Signed-off-by: lpt <141250073@smail.nju.edu.cn>

--- src/main/java/data/MergeDataImpl.java
+++ src/main/java/data/MergeDataImpl.java
@@ -77,7 +77,7 @@ public class MergeDataImpl implements MergeDataService {
 		List<ReportPO> list = new ArrayList<ReportPO>();
 		Connection connection = DBManager.connect();
 		PreparedStatement pStatement = null;
-		String sql = "SELECT * FROM report WHERE tname = '" + taskName + "' and state <>'1' order by location desc";
+		String sql = "SELECT * FROM report WHERE tname = '" + taskName + "' and state <>'1' order by location ";
 		pStatement = connection.prepareStatement(sql);
 		ResultSet rSet = pStatement.executeQuery();
 		while (rSet.next()) {
