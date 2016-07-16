f57166046b33bc41883facb2ada25f1284d41c0e
mySmileHan <649628511@qq.com>
2016-07-16 17:29:29 +0800

    development

--- src/main/java/data/SplitDataImpl.java
+++ src/main/java/data/SplitDataImpl.java
@@ -71,6 +71,13 @@ public class SplitDataImpl implements SplitDataService {
 			}
 		}
 
+		String sql5 = "SELECT * FROM merge WHERE final_id = " + id;
+		rSet = statement.executeQuery(sql5);
+		if(!rSet.next()) {
+			String sql6 = "DELETE FROM report WHERE id = " + id;
+			int i = statement.executeUpdate(sql6);
+			if(i==0) return false;
+		}
 		DBManager.stopAll(rSet, statement, connection);
 		return true;
 	}
