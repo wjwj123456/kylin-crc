f4da8b9cb877af9c63836f76129547a30eb07c0e
ldk14 <141250075@smail.nju.edu.cn>
2016-07-19 10:53:32 +0800

    增加最终报告内容

--- src/main/java/vo/AssessmentVO.java
+++ src/main/java/vo/AssessmentVO.java
@@ -9,17 +9,25 @@ public class AssessmentVO implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
 	private String reviewerName;
 	private int assessfaults_mt;
 	private int assessfaults_mh;
 	private int findedfaults;
+	private int uniquefaults;
+	private double time;
+	private int faultsperhour;
 
-	public AssessmentVO(String reviewerName, int assessfaults_mt, int assessfaults_mh, int findedfaults) {
+	public AssessmentVO(String reviewerName, int assessfaults_mt, int assessfaults_mh, int findedfaults,
+			int uniquefaults, double time, int faultsperhour) {
 		super();
 		this.reviewerName = reviewerName;
 		this.assessfaults_mt = assessfaults_mt;
 		this.assessfaults_mh = assessfaults_mh;
 		this.findedfaults = findedfaults;
+		this.uniquefaults = uniquefaults;
+		this.time = time;
+		this.faultsperhour = faultsperhour;
 	}
 
 	public AssessmentVO(AssessmentPO po) {
@@ -28,6 +36,33 @@ public class AssessmentVO implements Serializable {
 		this.assessfaults_mt = po.getAssessfaults_mt();
 		this.assessfaults_mh = po.getAssessfaults_mh();
 		this.findedfaults = po.getFindedfaults();
+		this.uniquefaults = po.getUniquefaults();
+		this.time = po.getTime();
+		this.faultsperhour = po.getFaultsperhour();
+	}
+
+	public int getUniquefaults() {
+		return uniquefaults;
+	}
+
+	public void setUniquefaults(int uniquefaults) {
+		this.uniquefaults = uniquefaults;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public int getFaultsperhour() {
+		return faultsperhour;
+	}
+
+	public void setFaultsperhour(int faultsperhour) {
+		this.faultsperhour = faultsperhour;
 	}
 
 	public String getReviewerName() {
@@ -61,4 +96,5 @@ public class AssessmentVO implements Serializable {
 	public void setFindedfaults(int findedfaults) {
 		this.findedfaults = findedfaults;
 	}
+
 }
