a21483cc37150da09bb3892ae2f217fa75c8b811
bedisdover <sym14@software.nju.edu.cn>
2016-07-22 20:22:31 +0800

    test

--- src/main/webapp/js/account.js
+++ src/main/webapp/js/account.js
@@ -4,24 +4,13 @@ $(function() {
 	for (var i = 0; i < languages.length; i++) {
 		$('#goodAt tbody').find('input[value=' + languages[i] + ']').attr('checked', 'checked');
 	}
+	
+	initFileInput('fileinput', '/crc/ImageServlet');
 })
 
-//初始化fileinput控件（第一次初始化）
-function initFileInput(ctrlName, uploadUrl) {    
-    var control = $('#' + ctrlName); 
-    control.fileinput({
-        language: 'zh', //设置语言
-        uploadUrl: uploadUrl, //上传的地址
-        allowedFileExtensions : ['jpg', 'png','gif'],//接收的文件后缀
-        showUpload: false, //是否显示上传按钮
-        showCaption: false,//是否显示标题
-        browseClass: "btn btn-primary", //按钮样式             
-        previewFileIcon: "<i class='glyphicon glyphicon-king'></i>", 
-    });
-}
-
 $('#confirm').on('click', function() {
-	confirm();
+//	confirm();
+	 $('#fileinput').fileinput('upload');
 });
 
 /**
