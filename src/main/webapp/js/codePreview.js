da741de4a65f950b5cc9aad8f7292dd939fdb7e8
origlin <236046350@qq.com>
2016-09-17 15:31:03 +0800

    Signed-off-by: origlin <236046350@qq.com>

--- src/main/webapp/js/codePreview.js
+++ src/main/webapp/js/codePreview.js
@@ -1,4 +1,5 @@
-SyntaxHighlighter.highlight();
+
+var fileName = "xxx.java";
 var settings = {
 	trigger : 'click',
 	width : 500,
@@ -7,19 +8,29 @@ var settings = {
 	style : '',
 	padding : true
 };
-function generateSetting(i,recordList) {
+function generateSetting(i, recordList) {
+	var deleteString ;
+	if(recordList[i-1]==''){
+		deleteString = '';
+	}else {
+		deleteString = "<button class = 'btn btn-new' onclick = 'deleteWithCode(this)'>删除</button>";
+	}
 	var settings = {
 		title : '第' + i + '行',
-		content : "<input class = 'form-control' value = "+recordList[i-1]+"><button class='btn pull-right'>提交</button>",
+		content : "<div class='form-group'><input class = 'form-control' value = "
+				+ recordList[i - 1]
+				+ ">"+deleteString+"<button class='btn pull-right' onclick='submitWithCode(this)'>提交</button></div>",
 	}
 	return settings;
 }
-function initButtons(length,recordList) {
+function initButtons(length, recordList) {
 	$('.gutter').css('cursor', 'pointer');
 	for (var i = 0; i < length + 1; i++) {
 		$($('.gutter').children()[i]).webuiPopover(
-				$.extend({}, settings, generateSetting(i+1,recordList)));
-		if(recordList[i]==''){
+				$.extend({}, settings, generateSetting(i + 1, recordList)));
+		if (recordList[i] == '') {
+			$($('.gutter').children()[i]).css("cssText",
+			"background-color:#fff!important");
 			$($('.gutter').children()[i]).hover(function() {
 				$(this).css("cssText", "background-color:#ddd!important");
 				$(this).text('+');
@@ -27,8 +38,9 @@ function initButtons(length,recordList) {
 				$(this).css("cssText", "background-color:#fff!important");
 				$(this).text($($('.gutter').children()).index($(this)) + 1);
 			});
-		}else {
-			$($('.gutter').children()[i]).css("cssText", "background-color:#eee!important");
+		} else {
+			$($('.gutter').children()[i]).css("cssText",
+					"background-color:#eee!important");
 			$($('.gutter').children()[i]).hover(function() {
 				$(this).css("cssText", "background-color:#ddd!important");
 				$(this).text('+');
@@ -39,7 +51,74 @@ function initButtons(length,recordList) {
 		}
 	}
 }
+function submitWithCode(obj) {
+	if ($(obj).parent().children('input').val() == '') {
+		$(obj).parent().addClass('has-error');
+	} else {
+		$(obj).parent().removeClass('has-error');
+		storeWithCode(obj);
+	}
+}
+function storeWithCode(obj) {
+	var number;
+	var numString = $(obj).parent().parent().parent().children('h3').text().trim();
+	numString = numString.replace('第', '').replace('行', '');
+	number = Number(numString.trim());
+	var report = new Object({
+		taskName : taskName,
+		fileName : fileName,
+		page : 0,
+		location : number,
+		description : $(obj).parent().children('input').val().trim(),
+		state : 0,
+		origin : 0
+	});
+	run_waitMe();
+    jQuery.ajax({
+        url: '/ReportServlet',
+        type: 'post',
+        data: 'type=store' + '&data=' + JSON.stringify(report),
+        success: function (data) {
+        	load();
+            stopWait(); 
+        },
+        error: function () {
+            alert('出错了，更改无法保存')
+            stopWait();
+        }
+    });
+}
+function deleteWithCode(obj) {
+	var number;
+	var numString = $(obj).parent().parent().parent().children('h3').text().trim();
+	numString = numString.replace('第', '').replace('行', '');
+	number = Number(numString.trim());
+	var report = new Object({
+        taskName: taskName,
+        userName: username,
+        fileName: fileName,
+        page: 0,
+        location: number,
+        description: $(obj).parent().children('input').val().trim(),
+        state: 0,
+        origin: 0
+    });
+	run_waitMe();
+    jQuery.ajax({
+        url: '/ReportServlet',
+        type: 'post',
+        data: 'type=delete' + '&data=' + JSON.stringify(report),
+        success: function (data) {
+        	load();
+            stopWait();
+        },
+        error: function () {
+            alert('出错了，更改无法保存')
+        }
+    });
+}
 function load() {
+	SyntaxHighlighter.highlight();
 	var fileName = "xxx.java";
 	var length = $('.gutter').children().length;
 	jQuery.ajax({
@@ -52,8 +131,9 @@ function load() {
 			var has;
 			for (var i = 0; i < length; i++) {
 				has = false;
-				for (var j = 0; j < tempData.length; j++) {	
-					if (tempData[j].fileName == fileName && tempData[j].location == i+1) {
+				for (var j = 0; j < tempData.length; j++) {
+					if (tempData[j].fileName == fileName
+							&& tempData[j].location == i + 1) {
 						recordList.push(tempData[j].description);
 						has = true;
 					}
@@ -62,13 +142,9 @@ function load() {
 					recordList.push('');
 				}
 			}
-			initButtons(length,recordList);
-			
+			initButtons(length, recordList);
+
 		}
 	});
-	
-	
-
-
 }
 load();
